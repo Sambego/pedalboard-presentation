108c4fa3cff4f62d324f09db44978ab970f80e77
Sambego <sambellen@gmail.com>
2016-05-12 13:31:06 +0200

    Add waveshaper oscilloscope example

--- js/createOscilloscopes.js
+++ js/createOscilloscopes.js
@@ -31,6 +31,62 @@ const destroyGainOscilloscope = () => {
 
 /* Oscillator
  * ---------------------------------- */
+const createWaveshaperOscilloscope = () => {
+    const waveshaperOscillator = audioContext.createOscillator();
+    const waveshaperOscilloscope = new Oscilloscope('.js-oscilloscope--waveshaper', audioContext);
+    const waveshaper = audioContext.createWaveShaper();
+    const calculateDistortionCurve = function(intens) {
+        const intensity = parseInt(intens) || 100,
+            amount = 44100,
+            deg = Math.PI / 180,
+            curve = new Float32Array(amount);
+        let i = 0,
+            x;
+
+        for (; i < amount; ++i ) {
+            x = i * 2 / amount - 1;
+            curve[i] = ( 3 + intensity ) * x * 20 * deg / ( Math.PI + intensity * Math.abs(x) );
+        }
+
+        return curve;
+    };
+
+
+    let toggleWaveshaper = false;
+
+    waveshaperOscillator.type = 'sine';
+    waveshaperOscillator.frequency.value = 400;
+    waveshaperOscillator.start();
+
+    waveshaper.curve = calculateDistortionCurve(150);
+
+    waveshaperOscillator.connect(waveshaperOscilloscope.analyserNode);
+
+    waveshaperOscilloscope.start();
+
+    document.querySelector('.js-button--waveshaper').addEventListener('mouseup', () => {
+        if (toggleWaveshaper) {
+            waveshaperOscillator.disconnect();
+            waveshaper.disconnect();
+
+            waveshaperOscillator.connect(waveshaperOscilloscope.analyserNode);
+        } else {
+            waveshaperOscillator.disconnect();
+
+            waveshaperOscillator.connect(waveshaper);
+            waveshaper.connect(waveshaperOscilloscope.analyserNode)
+        }
+
+        toggleWaveshaper = !toggleWaveshaper;
+    });
+}
+
+const destroyWaveshaperOscilloscope = () => {
+    document.querySelector('.js-oscilloscope--waveshaper').innerHTML = '';
+}
+
+/* Oscillator
+ * ---------------------------------- */
 const createOscilloscope = () => {
     const oscillator = audioContext.createOscillator();
     const oscilloscope = new Oscilloscope('.js-oscilloscope--oscillator', audioContext);
